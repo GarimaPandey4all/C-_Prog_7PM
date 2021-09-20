d08585ba8a1ae579413f87e873ba556bd9d0b736
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-09-20 21:56:33 +0530

    Add files via upload

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+
+// # - Pre-Processor Directive/folder
+//iostream: Header File
+#include <iostream> // iostream : input output stream (flow of data)
+
+using namespace std;
+
+//int main() // predefined function, entry point
+void main() // void - null or empty
+//int - return type
+// void - no return
+{
+    //std::cout << "Hello world!" << std::endl;
+    cout<<"Brain Mentors"<<endl; // print the output on the output screen
+    //return 0;
+}
